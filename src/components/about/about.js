007bc047506990d7fe2c0ad6a14208868dfb8035
dulceone <dulceonejs@gmail.com>
2021-06-08 22:16:11 +0300

    Change about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from './about.module.css';
+import styles from './about.module.scss';
+import aboutImage1 from '../../resources/about_image1.jpg';
 import artboardOne from '../../resources/Artboard1leaf1.png';
 import artboardTwo from '../../resources/Artboard1leaf2.png';
 import artboardFree from '../../resources/Artboard1leaf3.png';
@@ -24,240 +25,57 @@ export const About = () =>
 {
 
     return (
-
-        <section>
-            <Header>
-
-            </Header>
-
-            <div className={styles.container}>
-
-                <div className={styles.titel1}>
-                    <h1> Why mindfulness in education?</h1>
-                </div>
-                <div className={styles.img1}>
-                    <img src={artboard671} />
-                </div>
-                <div className={styles.content}>
-                    Mindfulness is the ability to be aware of
-                    what is happening in the present
-                    moment,without any judgement. It is a
-                    basic human ability that everyone has,
-                    that we can cultivate and train by prac
-                    tice. During mindfulness exercises,we
-                    are invited to pay attention to thoughts,
-                    sounds,smell,observations,emotions,
-                    parts of the body or the sensations of
-                    breathing, and to bring our attention
-                    back to the object of focus whenever the
-                    mind starts to wander. Another form of
-                    exercise is not using a specific focus yet
-                    training to stay aware of where the attention
-                    moves to. A third group of exercises
-                    are often called 'informal exercises':
-                    we train ourselves by paying more
-                    attention to specific actions, situations
-                    and interactions in our daily lives. For
-                    example, we drink tea, brush our hair, or
-                    walk to the next room with our full
-                    awareness.
-
-                    With guidance and training, mindfulness
-                    can develop into a way of living that
-                    brings greater focus and effectiveness as
-                    well as kindness and caring in everything
-                    we do.
-
-                    Childhood is an important formative and
-                    developmental stage that lays the foundation
-                    for well-being and mental health
-                    in later years and adulthood. The
-                    number of school-based programmes
-                    that aim to promote well-being and
-                    mindfulness, support emotional and
-                    social learning and prevent mental
-                    health problems in adulthood,is growing.
-</div>
-                <div className={styles.img2}>
-                    <img src={artboard63} />
-                </div>
-                <div className={styles.gallery1}>
-                    <div style={{
-                        "width": "60%",
-                        "padding-left": "50px",
-                        "padding-top": "50px"
-                    }}>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div></div>
-                    <div>
-                        <img src={artboardFour} />
-                    </div>
-                    <div style={{
-                        "margin-top": "0px"
-                    }}>
-                        <img src={artboardNine} />
-                    </div>
-                    <div>
-                        <img src={artboardSeven} />
-                    </div>
-                    <div></div>
-                    <div></div>
-                    <div>
-                        <img src={artboard69} />
-                    </div>
-                    <div style={{
-                        "width": "40%",
-                        "padding-left": "16px",
-                        "padding-top": "1px"
-                    }}>
-                        <img src={artboard21} />
-                    </div>
-                    <div>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div>
-                        <img src={artboardSix} />
-                    </div>
-                    <div style={{
-                        "width": "110%",
-                        "padding-bottom": "35px"
-                    }}>
-                        <img src={artboardSeven} />
-                    </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className={styles.titel2}>
-                    <h1> About the A.M.I.E. Project</h1>
-                </div>
-                <div className={styles.content1}>
-                    Arts and Mindfulness in Education
-                    (A.M.i.E.) is an international project,
-                    implemented in partnership with the Erasmus+
-                    programme. The aim of the project is to
-                    provide teachers worldwide with inspirational,
-                    innovative and supportive tools
-                    aiding the integration of mindfulness in
-                    primary and pre-primary curricula in an
-                    easy, fun and sustainable way.
-
-                    The unique feature of the A.M.i.E. project
-                    is the fact that it uses the power of the
-                    arts and creative processes to cultivate
-                    mindfulness in primary and pre-primary
-                    school environments.
-</div>
-                <div className={styles.gallery2}>
-                    <div>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div>
-                        <img src={artboardFree} />
-                    </div>
-                    <div></div>
-                    <div></div>
-                    <div>
-                        <img src={artboard67} />
-                    </div>
-                    <div>
-                        <img src={artboardNine} />
-                    </div>
-                    <div>
-                        <img src={artboard91} />
-                    </div>
-                    <div>
-                        <img src={artboardFive} />
-                    </div>
-                    <div></div>
-                </div>
-                <div className={styles.gallery3}>
-                    <div style={{ "padding-top": "170px" }}>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div>
-                        <img src={artboard68} />
-                    </div>
-                    <div style={{
-                        "padding-top": "40px"
-                    }}>
-                        <img src={artboardTwo} />
-                    </div>
-                </div>
-                <div className={styles.titel3}>
-                    <h1>What is Calminder?</h1>
-                </div>
-                <div className={styles.content2}>
-                    The name "Calminder" shows the different
-                    elements of the project. You can recognize
-                    the words:calendar, mind and calm,
-                    which all are related to mindfulness and
-                    the A.M.i.E. project itself.
-
-                    Calminder is an innovative and interactive
-                    web-based application that serves as an
-                    inspirational tool for teachers in pre-primary
-                    and primary education for introducing
-                    mindfulness through arts in their daily
-                    educational contexts in the form of a
-                    weekly calendar containing 'ready-to-go'
-                    exercises.
-
-                    Interdisciplinary and internationally
-                    composed student teams developed this
-                    multi-year-based project website under
-                    the guidance of Irma Smegen, founder of
-                    the company Speel je wijs. Mindfulness
-                    training sessions were held with the stu-
-                    dents, for a more in-depth perspective
-                    about how mindfulness works. These
-                    trainings were guided by specialised
-                    mindfulness trainers.
-
-                    More information, tools and exercises can
-                    be found in Irma Smegen's books which
-                    will be published the coming years.
-
-                    (Maybe you can add a link to 'mindful at
-                    school' here? <a target="_blank" href="https://www.irmasmegen.com/en/mindful-at-school/">
-                    www.mindfulatschool.com </a>
-                    and make it workable for us so we can add
-                    book titles as soon as they are available.)
-</div>
-                <div className={styles.gallery4}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div style={{
-                        "padding-top": "70px"
-                    }}>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div>
-                        <img src={artboard78} />
-                    </div>
-                    <div style={{ "padding-top": "170px" }}>
-                        <img src={artboardTwo} />
-                    </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
+    <div className={styles.wrapper}>
+        <section className={styles.first_screen}>
+            <div className={styles.title}>
+                Why mindfulness in enducation?
             </div>
 
+            <div className={styles.left}>
+                <p>Mindfulness is the ability to be aware of
+                what is happening in the present
+                moment,without any judgement. It is a
+                basic human ability that everyone has,
+                that we can cultivate and train by prac
+                tice. During mindfulness exercises,we
+                are invited to pay attention to thoughts,
+                sounds,smell,observations,emotions,
+                parts of the body or the sensations of
+                breathing, and to bring our attention
+                back to the object of focus whenever the
+                mind starts to wander. Another form of
+                exercise is not using a specific focus yet
+                training to stay aware of where the attention
+                moves to. A third group of exercises
+                are often called 'informal exercises':
+                we train ourselves by paying more
+                attention to specific actions, situations
+                and interactions in our daily lives. For
+                example, we drink tea, brush our hair, or
+                walk to the next room with our full
+                awareness.</p>
+
+                <p>With guidance and training, mindfulness
+                can develop into a way of living that
+                brings greater focus and effectiveness as
+                well as kindness and caring in everything
+                we do.</p>
+
+                <p>Childhood is an important formative and
+                developmental stage that lays the foundation
+                for well-being and mental health
+                in later years and adulthood. The
+                number of school-based programmes
+                that aim to promote well-being and
+                mindfulness, support emotional and
+                social learning and prevent mental
+                health problems in adulthood,is growing.</p>
+            </div>
+            <div className={styles.right}>
+                <img src={artboard63} />
+                <img src={aboutImage1} />
+            </div>
         </section>
-
-
+    </div>
     )
 };
